beecb889259e11a4e7441a77df2d0f593887b8d9
animasana <animagig@gmail.com>
2023-02-19 06:04:48 +0900

    spaces

--- src/Lotto.js
+++ src/Lotto.js
@@ -1,57 +1,57 @@
 export default class Lotto {
-	#nRows;
-	#nCols;
-	#nSize;
-	#matrix;
+  #nRows;
+  #nCols;
+  #nSize;
+  #matrix;
 
-	constructor(nRows = 5, nCols = 6, nSize = 45) {
-		this.#nRows = nRows;
-		this.#nCols = nCols;
-		this.#nSize = nSize;
+  constructor(nRows = 5, nCols = 6, nSize = 45) {
+    this.#nRows = nRows;
+    this.#nCols = nCols;
+    this.#nSize = nSize;
 
-		this.#matrix = new Array(nRows).fill(null).map(() => new Array(nCols));
-	}
+    this.#matrix = new Array(nRows).fill(null).map(() => new Array(nCols));
+  }
 
-	#sample() {
-		if (this.#nRows <= 0 || this.#nCols <= 0 || this.#nSize < this.#nCols) {
-			throw Error("Illegal Argument!!!");
-		}
+  #sample() {
+    if (this.#nRows <= 0 || this.#nCols <= 0 || this.#nSize < this.#nCols) {
+      throw Error("Illegal Argument!!!");
+    }
 
-		for (let i = 0; i < this.#matrix.length; i++) {
-			const seq = Array(this.#nSize)
-				.fill()
-				.map((_, i) => i + 1);
-			for (let j = 0; j < this.#matrix[i].length; j++) {
-				let drawnIndex = Math.floor(seq.length * Math.random());
-				this.#matrix[i][j] = seq.splice(drawnIndex, 1)[0];
-			}
-		}
-	}
+    for (let i = 0; i < this.#matrix.length; i++) {
+      const seq = Array(this.#nSize)
+        .fill()
+        .map((_, i) => i + 1);
+      for (let j = 0; j < this.#matrix[i].length; j++) {
+        let drawnIndex = Math.floor(seq.length * Math.random());
+        this.#matrix[i][j] = seq.splice(drawnIndex, 1)[0];
+      }
+    }
+  }
 
-	#sort() {
-		this.#matrix.forEach((row) => row.sort((a, b) => a - b));
-	}
+  #sort() {
+    this.#matrix.forEach((row) => row.sort((a, b) => a - b));
+  }
 
-	#matrixToString() {
-		const digit = String(this.#nSize).length;
+  #matrixToString() {
+    const digit = String(this.#nSize).length;
 
-		return this.#matrix
-			.map((aRow) =>
-				aRow
-					.map(String)
-					.map((v) => v.padStart(digit, "0"))
-					.join(" ")
-			)
-			.join("\n");
-	}
+    return this.#matrix
+      .map((aRow) =>
+        aRow
+          .map(String)
+          .map((v) => v.padStart(digit, "0"))
+          .join(" ")
+      )
+      .join("\n");
+  }
 
-	str() {
-		try {
-			this.#sample();
-			this.#sort();
-			return this.#matrixToString();
-		} catch (e) {
-			return "" + e;
-		}
-	}
+  str() {
+    try {
+      this.#sample();
+      this.#sort();
+      return this.#matrixToString();
+    } catch (e) {
+      return "" + e;
+    }
+  }
 }
--- src/Lotto01.js
+++ src/Lotto01.js
@@ -1,63 +1,63 @@
 export default class Lotto {
-	#nRows;
-	#nCols;
-	#nSize;
-	#matrix;
+  #nRows;
+  #nCols;
+  #nSize;
+  #matrix;
 
-	constructor(nRows = 5, nCols = 6, nSize = 45) {
-		this.#nRows = nRows;
-		this.#nCols = nCols;
-		this.#nSize = nSize;
+  constructor(nRows = 5, nCols = 6, nSize = 45) {
+    this.#nRows = nRows;
+    this.#nCols = nCols;
+    this.#nSize = nSize;
 
-		this.#matrix = this.#make();
-	}
+    this.#matrix = this.#make();
+  }
 
-	#make() {
-		if (
-			this.#nRows < 1 ||
-			this.#nCols < 1 ||
-			this.#nSize < 1 ||
-			this.#nSize < this.#nCols
-		) {
-			throw Error("Illegal Argument!!!");
-		}
+  #make() {
+    if (
+      this.#nRows < 1 ||
+      this.#nCols < 1 ||
+      this.#nSize < 1 ||
+      this.#nSize < this.#nCols
+    ) {
+      throw Error("Illegal Argument!!!");
+    }
 
-		let matrix = new Array(this.#nRows)
-			.fill()
-			.map(() => new Array(this.#nCols));
-      
+    let matrix = new Array(this.#nRows)
+      .fill()
+      .map(() => new Array(this.#nCols));
+
     matrix.forEach((_, i) => {
       matrix[i] = Array.from(this.#sample());
     });
-		matrix.forEach((row) => row.sort((a, b) => a - b));
+    matrix.forEach((row) => row.sort((a, b) => a - b));
 
-		return matrix;
-	}
+    return matrix;
+  }
 
-	*#seq(nSize) {
-		for (let i = 0; i < nSize; i++) {
-			yield i + 1;
-		}
-	}
+  *#seq(nSize) {
+    for (let i = 0; i < nSize; i++) {
+      yield i + 1;
+    }
+  }
 
-	*#sample() {
-		const seq = Array.from(this.#seq(this.#nSize));
+  *#sample() {
+    const seq = Array.from(this.#seq(this.#nSize));
 
-		for (let i = 0; i < this.#nCols; i++) {
-			yield seq.splice(Math.floor(seq.length * Math.random()), 1)[0];
-		}
-	}
+    for (let i = 0; i < this.#nCols; i++) {
+      yield seq.splice(Math.floor(seq.length * Math.random()), 1)[0];
+    }
+  }
 
-	#convert() {
-		const digits = String(this.#nSize).length;
-		const pad = (n) => n.padStart(digits, "0");
+  #convert() {
+    const digits = String(this.#nSize).length;
+    const pad = (n) => n.padStart(digits, "0");
 
-		return this.#matrix
-			.map((row) => row.map(String).map(pad).join(" "))
-			.join("\n");
-	}
+    return this.#matrix
+      .map((row) => row.map(String).map(pad).join(" "))
+      .join("\n");
+  }
 
-	str() {
-		return this.#convert();
-	}
+  str() {
+    return this.#convert();
+  }
 }
