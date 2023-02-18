fe52b61ff4259704c738b0361357408f2178e422
animasana <animagig@gmail.com>
2023-02-19 05:55:54 +0900

    space

--- src/LottoReactHooks.js
+++ src/LottoReactHooks.js
@@ -8,37 +8,37 @@ const mm = date.getMonth() + 1;
 const dd = date.getDate();
 
 function lottoToLines(lotto) {
-	return lotto.split("\n").map((row, index) => (
-		<span key={"line-" + index}>
-			{row} <br />
-		</span>
-	));
+  return lotto.split("\n").map((row, index) => (
+    <span key={"line-" + index}>
+      {row} <br />
+    </span>
+  ));
 }
 
 function LottoReactHooks({ name }) {
-	const [lotto, setLotto] = useState(new Lotto().str());
+  const [lotto, setLotto] = useState(new Lotto().str());
 
-	const handleClick = (event) => {
-		event.preventDefault();
-		setLotto(new Lotto().str());
-	};
+  const handleClick = (event) => {
+    event.preventDefault();
+    setLotto(new Lotto().str());
+  };
 
-	return (
-		<>
-			<div className="lottoStyle">
-				<span>
-					{yyyy}년 {mm}월 {dd}일
-				</span>
-				<hr />
-				<span>{name}</span>
-				<hr />
-				<span>{lottoToLines(lotto)}</span>
-			</div>
-			<div className="buttonStyle">
-				<button onClick={handleClick}>로또 재발행</button>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="lottoStyle">
+        <span>
+          {yyyy}년 {mm}월 {dd}일
+        </span>
+        <hr />
+        <span>{name}</span>
+        <hr />
+        <span>{lottoToLines(lotto)}</span>
+      </div>
+      <div className="buttonStyle">
+        <button onClick={handleClick}>로또 재발행</button>
+      </div>
+    </>
+  );
 }
 
 export default LottoReactHooks;
