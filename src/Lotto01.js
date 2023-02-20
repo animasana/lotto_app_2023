940db86ef3f90b509bfb9acc15a589971cc86ad5
animasana <animagig@gmail.com>
2023-02-20 12:39:41 +0900

    mapping

--- src/Lotto01.js
+++ src/Lotto01.js
@@ -51,8 +51,9 @@ export default class Lotto {
   #convert() {
     const digits = String(this.#nSize).length;
     const pad = (n) => String(n).padStart(digits, "0");
+    const makeString = (row) => row.map(pad).join(" ");
 
-    return this.#matrix.map((row) => row.map(pad).join(" ")).join("\n");
+    return this.#matrix.map(makeString).join("\n");
   }
 
   str() {
